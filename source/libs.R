11723dd3bdb5ae679c5d71ea2411f6e1d3d1c84f
moravecv <>
2019-07-04 15:43:36 +0200

    Fuctions added

--- source/libs.R
+++ source/libs.R
@@ -11,3 +11,20 @@ library(gridExtra)
 library(ggrepel)
 library(viridis)
 
+###############################
+
+create_labs <- function(n){
+  a <- 2018 - (ceiling(253/n) * n) 
+  b <- 2018
+  sq <- seq(from = a, to = b, by = n)
+  
+  labs <- c()
+  for (i in 1:(length(sq)-1)){
+    l <- paste0(sq[i] + 1, "-", sq[i] + n)
+    labs <- append(labs, l)
+  }
+  res <- list(labs, a, sq)
+  res
+}
+
+pp <- function(p, N){((p - 0.3)/(N + 0.4))}
